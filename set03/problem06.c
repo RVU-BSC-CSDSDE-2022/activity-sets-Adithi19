90abc60bf6c3dfe769b470ee003278dd1d4a4c87
Adithi  Vaidyanathan <114479214+Adithi19@users.noreply.github.com>
2022-12-07 18:44:33 +0000

    Problem solved

---
+++ set03/problem06.c
@@ -0,0 +1,48 @@
+//To find the index of a substring of a string
+
+#include <stdio.h>
+
+void input_string(char* a, char* b);
+int sub_str_index(char* string, char* substring);
+void output(char *string, char *substring, int index);
+
+int main() {
+  char* a[100];
+  char* b[100];
+  int index;
+  input_string(&a,&b);
+  index=sub_str_index(a,b);
+  output(a,b,index);
+  return 0;
+}
+
+void input_string(char* a, char* b) {
+  printf("Enter a string: ");
+  scanf("%s",a);
+  printf("Enter the substring: ");
+  scanf("%s",b);
+}
+
+int sub_str_index(char* string, char* substring) {
+  int i,k,position,index;
+  position=0;
+  for(i=0;string[i]!='\0';i++) {
+    index=0;
+    if(string[i]==substring[0]) {
+      for(k=i;substring[index]!='\0';k++) {
+        if(string[k]==substring[index]) {
+          position=i;
+        }
+        else {
+          break;
+        }
+        index++;
+      }
+    }
+  }
+  return position;
+}
+
+void output(char *string, char *substring, int index) {
+  printf("The index of %s in %s is %d",string,substring,index);
+}
