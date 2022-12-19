e250453a1ef8253f31ce9937a033dc6a335edee7
Adithi  Vaidyanathan <114479214+Adithi19@users.noreply.github.com>
2022-12-19 09:53:44 +0000

    Problem solved

--- set04/problem04.c
+++ set04/problem04.c
@@ -10,43 +10,54 @@ float evaluate_polynomial(int n, float a[n], float x);
 void output(int n, float a[n], float x, float result);
 
 int main() {
-  int n;
-  float x,result;
-  n=input_degree();
-  float a[n];
-  input_coefficients(n,a);
-  x=input_x();
-  result=evaluate_polynomial(n,a,x);
-  output(n,a,x,result);
-  return 0;
+  int n;
+  float x,result;
+  n=input_degree();
+  float a[n];
+  input_coefficients(n,a);
+  x=input_x();
+  result=evaluate_polynomial(n,a,x);
+  output(n,a,x,result);
+  return 0;
 }
 
 int input_degree() {
-  int degree;
-  printf("Enter the degree of the polynomial: ");
-  scanf("%d",&degree);
-  return degree;
+  int degree;
+  printf("Enter the degree: ");
+  scanf("%d",&degree);
+  return degree;
 }
 
 void input_coefficients(int n, float a[n]) {
-  int i;
-  for(i=0;i<n;i++) {
-    printf("Enter the coefficient: ");
-    scanf("%f",&a[i]);
-  }
+  int i;
+  for(i=0;i<n+1;i++) {
+    printf("Enter a number: ");
+    scanf("%f",&a[i]);
+  }
 }
 
 float input_x() {
-  float x_val;
-  printf("Enter the value of x: ");
-  scanf("%f",&x_val);
-  return x_val;
+  float x_val;
+  printf("Enter the value of x: ");
+  scanf("%f",&x_val);
+  return x_val;
 }
 
 float evaluate_polynomial(int n, float a[n], float x) {
-  float res;
- 
+  int i;
+  float res;
+  res = 0;
+  for(i=0;i<n+1;i++) {
+    res = res + (pow(a[i],i) * x);
+  }
+  return res;
 }
 
 void output(int n, float a[n], float x, float result) {
-  
+  int i;
+  printf("H(%d, ",n);
+  for(i=0;i<n;i++) {
+    printf("%.0f, ",a[i]);
+  }
+  printf("%.0f) = %.0f",a[i],result);
+}
